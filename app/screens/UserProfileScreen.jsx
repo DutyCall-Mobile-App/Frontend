cbc074dd53b026c55412438162cd6d2cd5c71f15
Kavishi Godage <it23313866@my.sliit.lk>
2025-10-11 16:59:20 +0530

    Revert "Fix chat feature"
    This reverts commit 1b0bfddb40d62316051e7b5111fc139bfd991f2e.

--- app/screens/UserProfileScreen.jsx
+++ app/screens/UserProfileScreen.jsx
@@ -1,27 +1,26 @@
-import { Ionicons } from '@expo/vector-icons';
-import { useRouter } from "expo-router";
-import { useEffect, useState } from 'react';
-import {
-  Alert,
-  Animated,
-  FlatList,
-  KeyboardAvoidingView,
+import React, { useEffect, useState } from 'react';
+import { 
+  View, 
+  Text, 
+  FlatList, 
+  TextInput, 
+  TouchableOpacity, 
+  StyleSheet, 
+  Alert, 
+  KeyboardAvoidingView, 
   Platform,
-  SafeAreaView,
-  StyleSheet,
-  Text,
-  TextInput,
-  TouchableOpacity,
-  View
+  Animated,
+  SafeAreaView
 } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import API from '../utils/api';
+import { Ionicons } from '@expo/vector-icons';
 
 export default function UserProfileScreen({ navigation }) {
   const [user, setUser] = useState(null);
   const [chat, setChat] = useState(null);
   const [text, setText] = useState('');
   const [expanded, setExpanded] = useState(false);
-  const router = useRouter();
   const animatedHeight = useState(new Animated.Value(0))[0];
 
   // Fetch user info
@@ -73,10 +72,9 @@ export default function UserProfileScreen({ navigation }) {
   };
 
   const handleLogout = async () => {
-    // await AsyncStorage.removeItem('token');
-    // await AsyncStorage.removeItem('role');
-    // navigation.reset({ index: 0, routes: [{ name: 'Login' }] });
-    router.push('/(tabs)');
+    await AsyncStorage.removeItem('token');
+    await AsyncStorage.removeItem('role');
+    navigation.reset({ index: 0, routes: [{ name: 'Login' }] });
   };
 
   const extraHeight = animatedHeight.interpolate({
