e538cb3661c4e768e443b825b56d4190b1648885
Kavishi Godage <it23313866@my.sliit.lk>
2025-10-11 17:11:40 +0530

    Revert "update .env"
    This reverts commit b8b9575ff5e2caf180f15d75060ee018c13ca76f.

--- app/report-form.jsx
+++ app/report-form.jsx
@@ -30,7 +30,6 @@ import {
   View,
 } from "react-native";
 import MapView, { Marker } from "react-native-maps";
-import { API_BASE } from "./utils/config";
 
 export default function ReportForm() {
   const router = useRouter();
@@ -78,7 +77,7 @@ export default function ReportForm() {
     Constants.expoConfig?.extra?.googleApiKey ||
     "AIzaSyB3tqIAvpAubH7frNjtrh3z8bWEsq0_zxY";
 
-  const API_URL = `${API_BASE}/reports/create`;
+  const API_URL = "http://172.20.10.9:3000/api/reports/create";
 
   // Language options for speech recognition
   const languageOptions = [
